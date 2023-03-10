9c9ff5f7a6455668bdcb6754886e5a89760b6495
DanBaDo <byte4life@pm.me>
2023-03-10 19:30:15 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<div>
+
+  <h1>Hola, {nome}</h1>
+
+  <input type="text" bind:value={nome}>
+  <p>{nome}</p>
+
+  <input type="text" bind:value={apelidos} >
+  <p>{apelidos}</p>
+
+  <p>{nomeCompleto}</p>
+
+  <button on:click={manexadorMostrar}>Mostrar?</button>
+
+  {#if (mostrar)}
+    <Componente propiedade={0}/>
+  {/if}
+
+</div>
+
+<script>
+  import Componente from "./Componente.svelte";
+
+  let nome = "Daniel"
+  let apelidos = "Bañobre"
+  let nomeCompleto
+  let mostrar = true
+
+  function manexadorMostrar() {
+    mostrar = !mostrar
+  }
+
+  $: {
+    nomeCompleto = `-> ${nome} ${apelidos}.`
+  } 
+</script>
---
+++ src/Componente.svelte
@@ -0,0 +1,11 @@
+<div>
+    <p>O valor da propiedade é {propiedade}</p>
+    <button on:click={manexadorClick}>+</button>
+</div>
+
+<script>
+    export let propiedade
+    function manexadorClick(){
+        propiedade++
+    }
+</script>
